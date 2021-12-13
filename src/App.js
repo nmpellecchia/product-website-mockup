f7dbc84ccedeed9296e809dcb083ac5f95e1b36b
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-12-13 17:18:50 -0300

    Fixed NavBar
    There were class / className and hyphen misstypes inside the svg elements

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Welcome from './components/Main/Welcome';
 import NavBar from './components/NavBar/NavBar.jsx';
 
 function App() {
   return (
     <div className="">
       <NavBar />
+      {<Welcome />}
     </div>
   );
 }
