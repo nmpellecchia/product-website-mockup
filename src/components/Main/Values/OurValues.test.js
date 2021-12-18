bef1baef099d27a9b35ecefba1f9a2e26b808f55
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-12-18 19:16:35 -0300

    Added value component tests removed app tests
    App component default tests was unnecesary. Added tests to the values components to see if it renders the amount I wanted

--- src/components/Main/Values/OurValues.test.js
+++ src/components/Main/Values/OurValues.test.js
@@ -3,9 +3,34 @@ import { screen, render } from '@testing-library/react';
 //
 import OurValues from './OurValues.jsx';
 
+////
+const testArr = [
+  {
+    icon: '',
+    name: 'test1',
+  },
+  {
+    icon: '',
+    name: 'test1',
+  },
+  {
+    icon: '',
+    name: 'test1',
+  },
+];
+
 describe('company values render testing', () => {
-  it('should display 6 values', () => {
-    render(<OurValues />);
-    /* expect(screen.getAllByRole('listitem')).toHaveLength(6); */
+  it('should display 3 values', () => {
+    render(<OurValues valuesArr={testArr} />);
+    expect(screen.getAllByRole('listitem')).toHaveLength(3);
+  });
+
+  it('should have text', () => {
+    render(<OurValues valuesArr={testArr} />);
+    expect(screen.getAllByText(testArr[0].name)).toBeTruthy();
+  });
+  it('should have an image', () => {
+    render(<OurValues valuesArr={testArr} />);
+    expect(screen.getAllByRole('img')).toBeTruthy();
   });
 });
