f4fd4265c5fc0055fbd01c2a76d689ecb393f817
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-12-18 19:15:21 -0300

    Moved images
    All images are now managed by the same component.

--- src/components/Main/Values/OurValues.jsx
+++ src/components/Main/Values/OurValues.jsx
@@ -1,51 +1,14 @@
 import React from 'react';
-//
-import trustImg from '../../imgs/trust.svg';
-import passionImg from '../../imgs/passion.svg';
-import satisImg from '../../imgs/satisfaction.svg';
-import responImg from '../../imgs/responsability.svg';
-import innovImg from '../../imgs/innovation.svg';
-import focusImg from '../../imgs/client_oriented.svg';
 
-function OurValues() {
-  const valuesArr = [
-    {
-      icon: trustImg,
-      name: 'trust',
-    },
-    {
-      icon: passionImg,
-      name: 'passion',
-    },
-    {
-      icon: satisImg,
-      name: 'satisfaction',
-    },
-    {
-      icon: responImg,
-      name: 'responsability',
-    },
-    {
-      icon: innovImg,
-      name: 'innovation',
-    },
-    {
-      icon: focusImg,
-      name: 'customer focus',
-    },
-  ];
-
+function OurValues(props) {
   return (
     <article>
       <ul>
-        {valuesArr.map(value => {
+        {props.valuesArr.map(value => {
           return (
             <li key={value.name}>
               <img src={value.icon} alt={`${value.icon} icon`} />
               <p>{value.name}</p>
-              <a href="#" className="a">
-                aa
-              </a>
             </li>
           );
         })}
--- src/components/Main/Welcome/Welcome.jsx
+++ src/components/Main/Welcome/Welcome.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import landImg from './land_img.jpg';
 
-function Welcome() {
+function Welcome(props) {
   return (
     <article
       className="w-full h-60 md:h-80 bg-center bg-cover flex items-center justify-center"
-      style={{ backgroundImage: `url(${landImg})` }}
+      style={{ backgroundImage: `url('${props.bgImage}')` }}
     >
       <h1 className="text-center text-5xl text-yellow-200 font-title">
         Our dream is to make <span className="text-pink-500">yours</span> true
