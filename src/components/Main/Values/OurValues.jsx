660b2b0d5df3eab99236eda11208e615f7d86756
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2021-12-19 17:46:53 -0300

    stylized OurValues component
    Changed the fill of the svgs to white. And removed the alt caption as this icons are decoratives and having them would hinder navigation.

--- src/components/Main/Values/OurValues.jsx
+++ src/components/Main/Values/OurValues.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 function OurValues(props) {
   return (
-    <article>
-      <ul>
+    <article className=" bg-yellow-500 ">
+      <h1 className="text-3xl font-title text-white ml-3 pt-3">
+        Our core values
+      </h1>
+      <ul className="grid grid-cols-3 gap-10 p-10">
         {props.valuesArr.map(value => {
           return (
-            <li key={value.name}>
-              <img src={value.icon} alt={`${value.icon} icon`} />
-              <p>{value.name}</p>
+            <li
+              key={value.name}
+              className="grid grid-cols-1 justify-items-center text-white text-xl"
+            >
+              <img src={value.icon} className="h-10 w-10" />
+              <p className="text-center pt-3">{value.name}</p>
             </li>
           );
         })}
